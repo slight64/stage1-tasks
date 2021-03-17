ead1d799d63e557934db44b0a0492153aa2a5795
slight64 <slight1989@gmail.com>
2021-03-17 16:31:39 +0400

    fix: create protection for key holding feat: complete switch function between letters and notes feat: add class notes to css for switch function refactor: move functions down in code

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,26 +1,3 @@
-function playNoteKey(e) {
-    const audio = document.querySelector(`audio[data-letter="${e.key}"]`);
-    const key = document.querySelector(`.piano-key[data-letter="${e.key}"]`);
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('piano-key-active');
-}
-function playClick(e) {
-  const selector = e.target.dataset.letter;
-  const target = e.target;
-  const audio = document.querySelector(`audio[data-letter="${selector}"]`);
-  audio.currentTime = 0;
-  audio.play();
-  target.classList.add('piano-key-active');
-}
-function removeTransition(e) {
-  if(e.propertyName !== 'transform') {
-    return ;
-  }
-  this.classList.remove('piano-key-active');
-}
-
 const keys = document.querySelectorAll('.piano-key');
 const switchBtn = document.querySelectorAll('.btn');
 
@@ -32,18 +9,58 @@ window.addEventListener('keydown', playNoteKey);
 
 switchBtn.forEach(el => el.addEventListener('click', switchMode))
 
+// Keyboard action
+function playNoteKey(e) {
+    eventKey = e.code.slice(-1);
+    const key = document.querySelector(`.piano-key[data-letter="${eventKey}"]`);
+    const audio = document.querySelector(`audio[data-letter="${eventKey}"]`);
+    if (!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+    // Check for key holding
+    key.classList.add('piano-key-active');
+    if (e.repeat) audio.currentTime = 1 ;
+}
+
+// Mouse action
+function playClick(e) {
+  const selector = e.target.dataset.letter;
+  const target = e.target;
+  const audio = document.querySelector(`audio[data-letter="${selector}"]`);
+  audio.currentTime = 0;
+  audio.play();
+  target.classList.add('piano-key-active');
+}
+
+// Switcher for Letter and Notes btn
 function switchMode(e) {
   const btnNotes = document.querySelector('.btn-notes');
   const btnLetters = document.querySelector('.btn-letters');
+
   if (e.target.className.includes('btn-notes')) {
-    btnNotes.classList.add('btn-active')
-    btnLetters.classList.remove('btn-active')
+    btnNotes.classList.add('btn-active');
+    btnLetters.classList.remove('btn-active');
+    keys.forEach(key => {
+      key.classList.add('notes');
+      key.classList.remove('letter');
+    })
   }
-  if (e.target.className.includes('btn-letters')) {
-    btnLetters.classList.add('btn-active')
-    btnNotes.classList.remove('btn-active')
+    if (e.target.className.includes('btn-letters')) {
+    btnLetters.classList.add('btn-active');
+    btnNotes.classList.remove('btn-active');
+    keys.forEach(key => {
+      key.classList.add('letter');
+      key.classList.remove('notes');
+    })
+
   }
 }
 
-
-
+// Remove transition from keys
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') {
+    return ;
+  }
+  this.classList.remove('piano-key-active');
+}
